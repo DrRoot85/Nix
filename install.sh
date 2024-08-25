c566b0853e7e62e174a4d307e0513fe1737152df
Mahmoud Hassan Ahmed <mhbmis@icloud.com>
2024-08-25 10:40:44 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-nix-shell -p git --command "nix run --experimental-features 'nix-command flakes' gitlab:librephoenix/nixos-config"
+#nix-shell -p git --command "nix run --experimental-features 'nix-command flakes' gitlab:librephoenix/nixos-config"
+
+yay -S hyprland-git gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus hyprlang hyprcursor hyprwayland-scanner xcb-util-errors hyprutils-git
+
